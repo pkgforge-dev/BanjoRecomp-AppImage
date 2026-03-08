aa4884bf978744a76a13cfc5964b559c3283b837
Link4Electronics <link4electronics@gmail.com>
2026-03-08 06:05:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,13 +3,13 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q banjorecomp | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/512x512/apps/banjorecomp.png
+export ICON=https://raw.githubusercontent.com/BanjoRecomp/BanjoRecomp/refs/heads/main/icons/app.png
 export DESKTOP=/usr/share/applications/banjorecomp.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /opt/banjorecomp/BanjoRecompiled
