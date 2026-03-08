b5de2153d3b4f3d6aee301eaa0f454addc9c8cf4
Link4Electronics <link4electronics@gmail.com>
2026-03-08 06:04:48 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,11 +16,23 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use X64 and ARM64 for the zip links
+	x86_64)  deb_arch=X64;;
+	aarch64) deb_arch=ARM64;;
+esac
+ZIP_LINK=$(wget https://api.github.com/repos/BanjoRecomp/BanjoRecomp/releases -O - \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*$deb_arch.zip")
+echo "$ZIP_LINK" | grep -oP 'v\K[\d.]+' > ~/version
+if ! wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+mkdir -p ./BanjoRecomp
+bsdtar -xvf /tmp/app.zip -C ./BanjoRecomp
+mv -v ./BanjoRecomp/BanjoRecompiled ./AppDir/bin
+mv -v ./BanjoRecomp/assets ./AppDir/bin
+wget -O ./AppDir/bin/recompcontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
