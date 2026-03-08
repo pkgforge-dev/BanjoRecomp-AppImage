bf51d0c3a2a7fe5cf8dc1562e9c0f1c78b50d95a
Link4Electronics <link4electronics@gmail.com>
2026-03-08 06:38:55 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -27,9 +27,7 @@ case "$ARCH" in # they use X64 and ARM64 for the zip links
 	aarch64) zip_arch=Linux-ARM64;;
 esac
 ZIP_LINK=$(wget -qO- https://api.github.com/repos/BanjoRecomp/BanjoRecomp/releases \
-      | sed 's/[()",{} ]/\n/g' \
-      | grep "https.*BanjoRecompiled.*$zip_arch\.zip" \
-      | head -n 1)
+      | sed 's/[()",{} ]/\n/g' | grep "https.*BanjoRecompiled.*$zip_arch\.zip" | head -n 1)
 echo "$ZIP_LINK" | grep -oP 'v\K[\d.]+' > ~/version
 if ! wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip 2>/tmp/download.log; then
 	cat /tmp/download.log
